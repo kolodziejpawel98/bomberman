74974cf46cf6fa3cc2cac287c8f25adfabca7984
Paweł Kołodziej <kolodziejpawel98@gmail.com>
2023-10-17 17:49:14 +0200

    animation sprites

--- global_variables.hpp
+++ global_variables.hpp
@@ -9,8 +9,8 @@
 #undef min
 #include <Arduino.h>
 
-const uint8_t screen_width = 80;
-const uint8_t screen_height = 64;
+const uint8_t SCREEN_WIDTH = 2 * 80;
+const uint8_t SCREEN_HEIGHT = 2 * 64;
 
 float player_x = 10;
 float player_y = 10;
---
+++ graphics.cpp
@@ -0,0 +1,9 @@
+#include "graphics.hpp"
+
+// Image characterWalkAnimation(characterWalkAnimationCode);
+Image background(backgroundCode);
+Image animationIdle(animationIdleCode);
+Image animationWalkDown(animationWalkDownCode);
+Image animationWalkLeft(animationWalkLeftCode);
+Image animationWalkRight(animationWalkRightCode);
+Image animationWalkUp(animationWalkUpCode);
