627c3ecd10a77d94504d4c6b1754c63b46fd4a99
Paweł Kołodziej <kolodziejpawel98@gmail.com>
2024-05-14 17:27:22 +0200

    cross neighbors

--- playground.hpp
+++ playground.hpp
@@ -163,12 +163,14 @@ namespace playground
 
     void setCrossPatternNeighbors()
     {
+        // 1st row
         cells[1]->crossPatternNeighbors.push_back(cells[10]);
         cells[3]->crossPatternNeighbors.push_back(cells[12]);
         cells[5]->crossPatternNeighbors.push_back(cells[14]);
         cells[7]->crossPatternNeighbors.push_back(cells[16]);
-
+        // 2nd row
         cells[9]->crossPatternNeighbors.push_back(cells[10]);
+
         cells[10]->crossPatternNeighbors.push_back(cells[1]);
         cells[10]->crossPatternNeighbors.push_back(cells[9]);
         cells[10]->crossPatternNeighbors.push_back(cells[19]);
@@ -184,6 +186,110 @@ namespace playground
 
         cells[13]->crossPatternNeighbors.push_back(cells[12]);
         cells[13]->crossPatternNeighbors.push_back(cells[14]);
+
+        cells[14]->crossPatternNeighbors.push_back(cells[5]);
+        cells[14]->crossPatternNeighbors.push_back(cells[13]);
+        cells[14]->crossPatternNeighbors.push_back(cells[23]);
+        cells[14]->crossPatternNeighbors.push_back(cells[15]);
+
+        cells[15]->crossPatternNeighbors.push_back(cells[16]);
+        cells[15]->crossPatternNeighbors.push_back(cells[14]);
+
+        cells[16]->crossPatternNeighbors.push_back(cells[7]);
+        cells[16]->crossPatternNeighbors.push_back(cells[15]);
+        cells[16]->crossPatternNeighbors.push_back(cells[25]);
+        cells[16]->crossPatternNeighbors.push_back(cells[17]);
+
+        cells[17]->crossPatternNeighbors.push_back(cells[16]);
+        // 3rd row
+        cells[19]->crossPatternNeighbors.push_back(cells[10]);
+        cells[19]->crossPatternNeighbors.push_back(cells[28]);
+        cells[21]->crossPatternNeighbors.push_back(cells[12]);
+        cells[21]->crossPatternNeighbors.push_back(cells[30]);
+        cells[23]->crossPatternNeighbors.push_back(cells[14]);
+        cells[23]->crossPatternNeighbors.push_back(cells[32]);
+        cells[25]->crossPatternNeighbors.push_back(cells[16]);
+        cells[25]->crossPatternNeighbors.push_back(cells[34]);
+        // 4th row
+        cells[27]->crossPatternNeighbors.push_back(cells[28]);
+
+        cells[28]->crossPatternNeighbors.push_back(cells[19]);
+        cells[28]->crossPatternNeighbors.push_back(cells[27]);
+        cells[28]->crossPatternNeighbors.push_back(cells[37]);
+        cells[28]->crossPatternNeighbors.push_back(cells[29]);
+
+        cells[29]->crossPatternNeighbors.push_back(cells[28]);
+        cells[29]->crossPatternNeighbors.push_back(cells[30]);
+
+        cells[30]->crossPatternNeighbors.push_back(cells[21]);
+        cells[30]->crossPatternNeighbors.push_back(cells[29]);
+        cells[30]->crossPatternNeighbors.push_back(cells[39]);
+        cells[30]->crossPatternNeighbors.push_back(cells[31]);
+
+        cells[31]->crossPatternNeighbors.push_back(cells[30]);
+        cells[31]->crossPatternNeighbors.push_back(cells[32]);
+
+        cells[32]->crossPatternNeighbors.push_back(cells[23]);
+        cells[32]->crossPatternNeighbors.push_back(cells[31]);
+        cells[32]->crossPatternNeighbors.push_back(cells[41]);
+        cells[32]->crossPatternNeighbors.push_back(cells[33]);
+
+        cells[33]->crossPatternNeighbors.push_back(cells[32]);
+        cells[33]->crossPatternNeighbors.push_back(cells[34]);
+
+        cells[34]->crossPatternNeighbors.push_back(cells[25]);
+        cells[34]->crossPatternNeighbors.push_back(cells[33]);
+        cells[34]->crossPatternNeighbors.push_back(cells[43]);
+        cells[34]->crossPatternNeighbors.push_back(cells[35]);
+
+        cells[35]->crossPatternNeighbors.push_back(cells[34]);
+        // 5th row
+        cells[37]->crossPatternNeighbors.push_back(cells[28]);
+        cells[37]->crossPatternNeighbors.push_back(cells[46]);
+        cells[39]->crossPatternNeighbors.push_back(cells[48]);
+        cells[39]->crossPatternNeighbors.push_back(cells[30]);
+        cells[41]->crossPatternNeighbors.push_back(cells[50]);
+        cells[41]->crossPatternNeighbors.push_back(cells[32]);
+        cells[43]->crossPatternNeighbors.push_back(cells[52]);
+        cells[43]->crossPatternNeighbors.push_back(cells[34]);
+        // 6th row
+        cells[45]->crossPatternNeighbors.push_back(cells[46]);
+
+        cells[46]->crossPatternNeighbors.push_back(cells[37]);
+        cells[46]->crossPatternNeighbors.push_back(cells[45]);
+        cells[46]->crossPatternNeighbors.push_back(cells[55]);
+        cells[46]->crossPatternNeighbors.push_back(cells[47]);
+
+        cells[47]->crossPatternNeighbors.push_back(cells[46]);
+        cells[47]->crossPatternNeighbors.push_back(cells[48]);
+
+        cells[48]->crossPatternNeighbors.push_back(cells[39]);
+        cells[48]->crossPatternNeighbors.push_back(cells[47]);
+        cells[48]->crossPatternNeighbors.push_back(cells[57]);
+        cells[48]->crossPatternNeighbors.push_back(cells[49]);
+
+        cells[49]->crossPatternNeighbors.push_back(cells[48]);
+        cells[49]->crossPatternNeighbors.push_back(cells[50]);
+
+        cells[50]->crossPatternNeighbors.push_back(cells[41]);
+        cells[50]->crossPatternNeighbors.push_back(cells[49]);
+        cells[50]->crossPatternNeighbors.push_back(cells[59]);
+        cells[50]->crossPatternNeighbors.push_back(cells[51]);
+
+        cells[51]->crossPatternNeighbors.push_back(cells[50]);
+        cells[51]->crossPatternNeighbors.push_back(cells[52]);
+
+        cells[52]->crossPatternNeighbors.push_back(cells[43]);
+        cells[52]->crossPatternNeighbors.push_back(cells[51]);
+        cells[52]->crossPatternNeighbors.push_back(cells[61]);
+        cells[52]->crossPatternNeighbors.push_back(cells[53]);
+
+        cells[53]->crossPatternNeighbors.push_back(cells[52]);
+        // 7th row
+        cells[55]->crossPatternNeighbors.push_back(cells[46]);
+        cells[57]->crossPatternNeighbors.push_back(cells[48]);
+        cells[59]->crossPatternNeighbors.push_back(cells[50]);
+        cells[61]->crossPatternNeighbors.push_back(cells[52]);
     }
 
     std::vector<std::shared_ptr<Cell>>::iterator findNearestCell(int playerXcoordinate, int playerYcoordinate)
